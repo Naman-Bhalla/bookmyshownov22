1fc4d02ed111680b2dd9b42b841b49c883049574
Naman Bhalla <namanbhalla1998@gmail.com>
2022-11-15 23:49:06 +0530

    End of classs

---
+++ src/main/java/com/scaler/bookmyshownov22/controllers/TicketController.java
@@ -0,0 +1,30 @@
+package com.scaler.bookmyshownov22.controllers;
+
+import com.scaler.bookmyshownov22.dtos.BookTicketRequestDto;
+import com.scaler.bookmyshownov22.dtos.BookTicketResponseDto;
+import com.scaler.bookmyshownov22.models.Ticket;
+import com.scaler.bookmyshownov22.services.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TicketController {
+    private TicketService ticketService;
+
+    @Autowired
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    public BookTicketResponseDto bookTicket(BookTicketRequestDto request) {
+        Ticket ticket = ticketService.bookTicket(
+                request.getShowSeatIds(),
+                request.getUserId(),
+                request.getShowId()
+        );
+
+
+
+        return null;
+    }
+}
